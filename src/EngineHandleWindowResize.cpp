889f68d7709b60939fa0d3c7f46247ca76a1729c
jli <coderguy50@gmail.com>
2022-11-21 18:04:10 -0600

    quick hotfix for minimizing windows

--- src/EngineHandleWindowResize.cpp
+++ src/EngineHandleWindowResize.cpp
@@ -4,7 +4,11 @@ void Engine::HandleWindowResize(GLFWwindow* win, int newWidth, int newHeight)
 {
 	const float fov = 75.0f;
 	//Update the aspect ratio
-	float aspect = (float)newWidth / (float)newHeight;
+	float aspect;	
+	if(newHeight == 0)
+		aspect = 1;	
+	else
+		aspect = (float)newWidth / (float)newHeight;
 	//Update the OpenGL viewport
 	glViewport(0, 0, newWidth, newHeight);
 	perspectiveMatrix = glm::perspective(3.1415926535f / 180.0f * fov, aspect, 0.1f, 1000.0f);
